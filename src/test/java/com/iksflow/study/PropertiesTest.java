f6e8f21ff7a974d63c56ef2f0926ec1f4cbd32e6
성승익 <seunglk.seong>
2020-08-04 17:40:51 +0900

    테스트코드 클래스, 프로퍼티 로드방식 학습

--- src/test/java/com/iksflow/study/PropertiesTest.java
+++ src/test/java/com/iksflow/study/PropertiesTest.java
@@ -1,16 +1,27 @@
 package com.iksflow.study;
 
+import com.iksflow.study.controller.BoardController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+// classes속성을 정의하게 되면 해당하는 클래스만 불러온다. 정의하지않으면 모든클래스를 대상으로한다.
+
+@SpringBootTest(classes= BoardController.class, properties = {"author.name=ssi"})
 public class PropertiesTest {
+
+    // Environment를 Autowired하면 application.properties의 정보를 가져온다.
+    @Autowired
+    Environment environment;
 
     @Test
     public void testMethod() {
-
+        System.out.println("이름 : " + environment.getProperty("author.name"));
+        System.out.println("나이 : " + environment.getProperty("author.age"));
+        System.out.println("국가 : " + environment.getProperty("author.nation"));
     }
 }
